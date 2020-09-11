9f1dec9f06dc8a5ee975141f59f1823e1df421e3
kongyoubuaa <18182648@buaa.edu.cn>
2020-09-11 08:04:52 +0800

    Lab0 branch

---
+++ lab0.rb
@@ -0,0 +1,11 @@
+# BUAA Ruby Programming Language 2020
+#
+# 对正整数n，用p(n)表示n的各位非0数字的乘积，如果n是个位数，则p(n)=n。求p(1)+p(2)+…+p(m)的最大质因数。
+# 写一个尽量简短的Ruby程序实现解决上述问题
+#
+# Author : Yixing Ma
+
+def mfp(m)
+
+
+end
