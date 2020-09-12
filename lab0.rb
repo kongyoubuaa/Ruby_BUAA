135d50199c13d2e59b7aa72a4bd2228da9e36c5b
kongyoubuaa <18182648@buaa.edu.cn>
2020-09-12 20:49:11 +0800

    Lab0 branch - complete lab0.rb

--- lab0.rb
+++ lab0.rb
@@ -5,6 +5,44 @@
 #
 # Author : Yixing Ma
 
-def mfp(m)
-  puts (1..m).map{|i| i.to_s.chars.map(&:to_i).select{|v| v>0}.inject(&:*) }.inject(&:+)
+# Calculate the max prime
+def primeFactors(number)
+    if number == 1
+        return 1
+    end
+    for i in 2...number
+      if number % i == 0
+          return (primeFactors(number/i))
+      end
+    end
+    return number
 end
+
+# Main function
+def mfp(m)
+  # Func to calculate the sum - from Internet
+  # puts (1..m).map{|i| i.to_s.chars.map(&:to_i).select{|v| v>0}.inject(&:*) }.inject(&:+)
+
+  # Calculate the sum
+  sum = 0
+  m = m + 1
+  for i in 1...m
+    temp = i
+    mul = 1
+    while temp > 0
+      bit = temp % 10
+      if bit != 0
+        mul = mul * bit
+      end
+      temp = temp / 10
+    end
+    sum = sum + mul
+  end
+  #puts sum
+
+  # output the value of the max prime
+  puts primeFactors(sum)
+
+end
+
+mfp(6)
